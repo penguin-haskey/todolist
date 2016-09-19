d4c2714b8d1eb6640ec60e2b900ac0250902e987
penguin <penguin+1@has-key.org>
2016-09-19 04:49:04 +0000

    first commit

---
+++ myapp.rb
@@ -0,0 +1,52 @@
+# myapp.rb
+require 'sinatra'
+require 'data_mapper'
+require 'sinatra/reloader'
+require 'date'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
+class Item
+    include DataMapper::Resource
+    property :id, Serial
+    property :content, Text, :required => true
+    property :done, Boolean, :required => true, :default => false
+    property :created, DateTime
+end
+    
+get '/' do
+  @items = Item.all(:order => :created.desc)
+  redirect '/new' if @items.empty?
+  erb :index
+end
+
+get '/new' do
+  @title = "Add todo item"
+  erb :new
+end
+
+post '/new' do
+  Item.create(:content => params[:content], :created => Time.now)
+  redirect '/'
+end
+
+get '/delete/:id' do
+  @item = Item.first(:id => params[:id])
+  erb :delete
+end
+
+post '/delete/:id' do
+  if params.has_key?("ok")
+    item = Item.first(:id => params[:id])
+    item.destroy
+    redirect '/'
+  else
+    redirect '/'
+  end
+end
+
+get '/done/:id' do
+  item = Item.first(:id => params[:id])
+  item.done = !item.done
+  item.save
+    redirect '/'
+end
